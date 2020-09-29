16ef827007070701873baa423af3a30d6aad3eef
olivier.bonheur <olivier.bonheur@gmail.com>
2020-09-30 00:07:43 +0200

    Work on controller

--- TourGuide/src/main/java/preferencesModule/controller/PreferencesController.java
+++ TourGuide/src/main/java/preferencesModule/controller/PreferencesController.java
@@ -3,10 +3,15 @@ package preferencesModule.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import preferencesModule.service.IPreferencesService;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import tripPricer.Provider;
 
 import java.util.List;
@@ -18,12 +23,31 @@ public class PreferencesController {
 
     @Autowired
     IPreferencesService preferencesService;
-
+/*
     @RequestMapping("/getPrice")
-    public List<Provider> getPrice(@RequestParam String apiKey, @RequestParam String attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints) {
+    public Mono<List<Provider>> getPrice(@RequestParam String apiKey, @RequestParam String attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints) {
         logger.debug("Request getPrice : ApiKey=" + apiKey + " - attractionId=" + attractionId + " - adults=" + adults + " - children=" + children + " - nightsStay=" + nightsStay + " - rewardsPoints=" + rewardsPoints);
         List<Provider> providers = preferencesService.getPrice(apiKey, UUID.fromString(attractionId), adults, children, nightsStay, rewardsPoints);
         logger.debug("Response Nb Providers=" + providers.size());
         return providers;
     }
+*/
+/*
+    @RequestMapping("/getPrice")
+    public Mono<ServerResponse> getPrice(@RequestParam String apiKey, @RequestParam String attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints) {
+        logger.debug("Request getPrice : ApiKey=" + apiKey + " - attractionId=" + attractionId + " - adults=" + adults + " - children=" + children + " - nightsStay=" + nightsStay + " - rewardsPoints=" + rewardsPoints);
+        List<Provider> providers = preferencesService.getPrice(apiKey, UUID.fromString(attractionId), adults, children, nightsStay, rewardsPoints);
+        logger.debug("Response Nb Providers=" + providers.size());
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).
+                body(BodyInserters.fromObject(providers));
+    }
+*/
+    @RequestMapping("/getPrice")
+    public Flux<Provider> getPrice(@RequestParam String apiKey, @RequestParam String attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints) {
+        logger.debug("Request getPrice : ApiKey=" + apiKey + " - attractionId=" + attractionId + " - adults=" + adults + " - children=" + children + " - nightsStay=" + nightsStay + " - rewardsPoints=" + rewardsPoints);
+        List<Provider> providers = preferencesService.getPrice(apiKey, UUID.fromString(attractionId), adults, children, nightsStay, rewardsPoints);
+        logger.debug("Response Nb Providers=" + providers.size());
+        return Flux.fromIterable(providers);
+    }
+
 }
